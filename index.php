5384013cb2b119b29c399af97f25098732817383
Bonsanto <xbonsx@gmail.com>
2015-03-18 19:33:01 -0430

    - Added php fle

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+<?php
+echo "<h1>Hello world</h1>";
+for ($i = 0; $i < 10; $i++) {
+    echo "<h1>", $i, "</h1>";
+}
+?>
+</body>
+</html>
