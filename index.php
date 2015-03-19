ec5153bbc2a2f99a8a5376208c2697a949e765b2
Jorge Sarmiento <doctor.n4z@gmail.com>
2015-03-18 20:06:53 -0430

    - Changed title. - Added i x 2

--- index.php
+++ index.php
@@ -2,14 +2,16 @@
 <html>
 <head lang="en">
     <meta charset="UTF-8">
-    <title></title>
+    <title>El proyectico :):(:S:P</title>
 </head>
 <body>
 <?php
 echo "<h1>Hello world</h1>";
 for ($i = 0; $i < 10; $i++) {
     echo "<h1>", $i, "</h1>";
+    echo $i*2;
 }
+
 ?>
 </body>
 </html>
