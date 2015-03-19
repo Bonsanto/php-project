6bb50ba304e2911246da2330cd34ae97e26c4453
Jorge Sarmiento <doctor.n4z@gmail.com>
2015-03-18 20:28:44 -0430

    - Added some dumb button

--- index.php
+++ index.php
@@ -5,11 +5,12 @@
     <title>El proyectico :):(:S:P</title>
 </head>
 <body>
+<input placeholder='pepito' type="text"/>
 <?php
 echo "<h1>Hello world</h1>";
 for ($i = 0; $i < 10; $i++) {
     echo "<h1>", $i, "</h1>";
-    echo $i*2;
+    echo $i * 2;
     echo "<p> VIVA FIDEL</p>";
 }
 
